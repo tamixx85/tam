5f8b5c3d6f9f3c96483ce81a280f3790ec02c28b
Tame <tamaraamiel85@gmail.com>
2020-09-09 17:31:33 -0300

    tam

---
+++ script.js
@@ -0,0 +1,8 @@
+function elegir (coca){
+    var gaseosa = document.querySelector(".gaseosa")
+    gaseosa. src =coca
+}
+function cambiarfondo (colorfondo){
+    var cuerpo =document.querySelector (".body")
+    cuerpo. style.background=colorfondo
+}
